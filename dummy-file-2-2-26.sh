2e6ff35c2bc246ad6d525903ea2a5aa1471948ef
mspshspsm <157770102+mspshspsm@users.noreply.github.com>
2026-02-06 15:20:58 -0500

    Create dummy-file-2-2-26.sh

---
+++ dummy-file-2-2-26.sh
@@ -0,0 +1,6 @@
+#dummy file
+Contains Synopsys proprietary information.
+Compiler version W-2024.09-SP2_Full64; Runtime version W-2024.09-SP2_Full64;  Jan 21 11:19 2026
+*Verdi* Loading libsscore_vcs202409.so
+FSDB Dumper for VCS, Release Verdi_X-2025.06-1, Linux x86_64/64bit, 07/19/2025
+(C) 1996 - 2025 by Synopsys, Inc.
