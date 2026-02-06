f3ce6a8c8847c67ae8b083fcc2f6e57f98265a2f
mspshspsm <157770102+mspshspsm@users.noreply.github.com>
2026-02-06 15:18:10 -0500

    Update dummy-file.sh

--- dummy-file.sh
+++ dummy-file.sh
@@ -1,1 +1,9 @@
 #dummy-file.sh
+Chronologic VCS (TM)
+       Version O-2018.09-SP2_Full64 -- Sat Nov 15 17:21:54 2025
+               Copyright (c) 1991-2018 by Synopsys Inc.
+                         ALL RIGHTS RESERVED
+
+This program is proprietary and confidential information of Synopsys Inc.
+and may be used and disclosed only as authorized in a license agreement
+controlling such use and disclosure.
